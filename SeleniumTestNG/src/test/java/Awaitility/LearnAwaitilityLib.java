3be90debe63cdfb83b21d3f6493e36b590f411c4
myevlentiev <yevlentiev.maksym@airslate.com>
2021-11-01 18:00:33 +0200

    Learn Awaitility Lib

--- SeleniumTestNG/src/test/java/Awaitility/LearnAwaitilityLib.java
+++ SeleniumTestNG/src/test/java/Awaitility/LearnAwaitilityLib.java
@@ -1,9 +1,10 @@
 package Awaitility;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -12,7 +13,10 @@ import static org.awaitility.Awaitility.await;
 
 public class LearnAwaitilityLib {
 
+    ChromeDriver driver = new ChromeDriver();
     private int countRun = 0;
+    private String URL = "https://auto.ria.com/uk/";
+    By sellBtn = By.cssSelector("a>[aria-label='Продати авто']");
 
     @Test
     public void awaitilityRun() {
@@ -27,5 +31,30 @@ public class LearnAwaitilityLib {
             return true;
         }
         return false;
+    }
+
+    @Test
+    public void awaitilityWithUntil() {
+        driver.get(URL);
+        await("Wait for the sell button to be displayed").atMost(30, TimeUnit.SECONDS)
+                .ignoreExceptions()
+                .until(driver.findElement(sellBtn)::isDisplayed);
+    }
+
+    @Test
+    public void awaitilityWithAssert() {
+        driver.get(URL);
+        await().atMost(30, TimeUnit.SECONDS)
+                .ignoreExceptions()
+                .untilAsserted(() -> Assert.assertEquals(driver.findElement(sellBtn).isDisplayed(), true,
+                        "Wait for the sell button to be displayed"));
+        driver.findElement(sellBtn).click();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
